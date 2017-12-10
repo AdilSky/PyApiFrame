7555a44718957c7152364d057f84e3d3587394ad
AdilSky <adilsky@aliyun.com>
2017-12-10 23:44:45 +0800

    增加 common 的 Excel操作类，以及另一种用例写法。

--- Wm_Api/test_case/login_v1_test.py
+++ Wm_Api/test_case/login_v1_test.py
@@ -8,7 +8,7 @@
 import unittest,requests
 
 from  Wm_Api.common import Excel
-import json
+
 
 Ex = Excel.Excel()
 
@@ -17,85 +17,44 @@ class LoginTest(unittest.TestCase):
 
     def setUp(self):
         self.base_url = 'https://www.yiyao.cc'
-        caseList = Ex.readExcel('ApiInfo.xlsx', 'Login')
 
-        for caseDict in caseList:
-            print(caseDict)
 
     def tearDown(self):
-        print(self.result)
-
-
-    def loginTest(self):
-        '''用户登录'''
-
-        caseList = Ex.readExcel('ApiInfo.xlsx','Login')
-        self.base_url = 'https://www.yiyao.cc'
-        for caseDict in caseList:
-            # print(caseDict)
-
-            url1 = self.base_url + caseDict['ApiLoad']
-            method = caseDict['Method']
-            caseData = caseDict['CaseData']
-            caseRun = caseDict['CaseRun']
-            print(url1)
-            if caseRun == 'Y':
-                if method == 'Post':
-                    print(caseDict['CaseName'])
-                    print(caseData)
-                    print(type(caseData))
-                    caseData = eval(caseData)
-                    print(type(caseData))
-                    # 下面是上面三条打印的结果，看似一样，但是类型不一样。所以需要转为dict
-                    # {'username': 'xzyc001', 'password': '111111'}
-                    # < class 'str'>
-                    #
-                    # < class 'dict'>
-
-                    dataList = list(caseData)
-                    print(dataList)
-                    # caseData = json.dumps(caseData)
-                    url = "https://www.yiyao.cc/user/loginWeb"
-                    values ={'username': 'xzyc001', 'password': '111111'}
-                    print(type(values))
-                    values = caseData
-                    # values = json.dumps(values)
-                    print(values)
-                    response = requests.post(url1, caseDict['CaseData'])
-                    self.result = response.json()
-                    self.assertEqual(self.result['success'],True,msg=None)
-                    self.assertEqual(self.result['error'],None,msg=None)
-                    self.assertEqual(self.result['message'],'登录成功',msg=None)
-                    print(self.result)
-                if method == 'Get':
-
-                    pass
-
-
-        # try:
-        #     url = self.base_url +
-        #     values = {'username':'xzyc001','password':'111111'}
-        #     # values['username'] = 'xzyc001'
-        #     # values['password'] = '111111'
-        #     # 临时解决https的问题
-        #     response = requests.post(url, values)
-        #     self.result = response.json()
-        #     self.assertEqual(self.result['success'],True,msg=None)
-        #     self.assertEqual(self.result['error'],None,msg=None)
-        #     self.assertEqual(self.result['message'],'登录成功',msg=None)
-        # except BaseException as msg:
-        #     self.assertIsNone(msg,msg=None)
-        #     print(msg)
-
-
-
-
         pass
 
 
+    def testLogin(self):
+        '''用户登录'''
+        try:
+            caseList = Ex.readExcel('ApiInfo.xlsx','Login')
+
+            for caseDict in caseList:
+                url = self.base_url + caseDict['ApiLoad']
+                method = caseDict['Method']
+                caseData = caseDict['CaseData']
+                caseRun = caseDict['CaseRun']
+                caseName = caseDict['CaseName']
+                expectValue = caseDict['ExpectValue']
+                caseData = eval(caseData)
+                expectValue = eval(expectValue)
+                if caseRun == 'Y':
+                    if method == 'Post':
+
+                        print(caseName)
+                        response = requests.post(url, caseData)
+                        self.result = response.json()
+                        self.assertEqual(self.result['success'],expectValue['success'],msg=None)
+                        self.assertEqual(self.result['error'],expectValue['error'],msg=None)
+                        self.assertEqual(self.result['message'],expectValue['message'],msg=None)
+                        print(self.result)
+
+                    if method == 'Get':
+                        pass
+        except BaseException as msg:
+            self.assertIsNone(msg, msg=None)
+            print(msg)
 
 
 if __name__ == '__main__':
 
-    loginTest = LoginTest()
-    loginTest.loginTest()
+    unittest.main()
