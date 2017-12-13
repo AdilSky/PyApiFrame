7246d297d2456310b0d61863a167ede78dd85d2d
AdilSky <adilsky@aliyun.com>
2017-12-13 21:27:31 +0800

    增加一个公共测试文件

---
+++ Wm_Api/test_case/WmApi_test.py
@@ -0,0 +1,25 @@
+# FileName : WmApi_test.py
+# Author   : Adil
+# DateTime : 2017/12/13 19:41
+# SoftWare : PyCharm
+
+import unittest
+
+
+
+class WmApiTest(unittest.TestCase):
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+
+
+
+
+
+
