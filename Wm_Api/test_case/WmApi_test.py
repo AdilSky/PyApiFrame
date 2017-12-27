dcf63b092d7d3b9a454234e9a77103bbd23adcff
AdilSky <adilsky@aliyun.com>
2017-12-27 17:29:22 +0800

    优化注释

--- Wm_Api/test_case/WmApi_test.py
+++ Wm_Api/test_case/WmApi_test.py
@@ -17,6 +17,7 @@ class WmApiTest(unittest.TestCase):
     def setUpClass(cls):
         cls.yiyaoURL = 'https://www.yiyao.cc'
         cls.bijiaURL =  'http://bijia.yiyao.cc'
+        cls.SpecialURL = 'http://sales-app-api.assistant'
         # 实例化 Ex
         cls.Ex = Excel.Excel()
 
@@ -25,8 +26,74 @@ class WmApiTest(unittest.TestCase):
 
         pass
 
+    def atestSpecial(self):
+        '''特派员'''
+        print('特派员', '\n')
 
-    def testComPrice(self):
+        resultList = []
+        titleList = []
+        caseList = self.Ex.readExcel('ApiInfo.xlsx', 'Special')
+        for caseDict in caseList:
+            url = self.SpecialURL + caseDict['ApiLoad']
+            titleList = list(caseDict)
+            caseNum = caseDict['CaseNum']
+            method = caseDict['Method']
+            caseData = caseDict['CaseData']   # 这样获取的数据 是str 类型的
+            if caseDict['CaseData'] != '':
+                caseDataDict = eval(caseData)              # 使用eval 转换为dict 类型
+            # 将dict类型转化为json类型
+            data = json.dumps(caseDataDict)
+            caseRun = caseDict['CaseRun']
+            caseName = caseDict['CaseName']
+            # 设置请求头
+            headers = caseDict['Header']
+            if headers !='':
+                headers = eval(headers)
+            # 设置报告打印内容
+            print('用例 ' + caseNum, end=': ')
+            print(caseName)
+            print('数据参数 : ' + caseData)
+            # print('搜索商品 : ' + caseDataDict['keyword'])
+            if caseRun == 'Y':
+                expectValue = eval(caseDict['ExpectValue'])
+                if method == 'Post':
+                    response = requests.post(url, data, headers=headers)
+                    result = response.json()
+                    # 设置断言
+                    if result['code'] == expectValue['code'] and result['msg'] == expectValue['msg']:
+                        caseDict['CaseResult'] = 'Pass'
+                    else:
+                        caseDict['CaseResult'] = 'Fail'
+                    print('返回结果', end=': ')
+                    print(result)
+                    print('执行结果',end=': ')
+                    print(caseDict['CaseResult'], '\n')
+                    # 这里要对结果进行一下处理，要不无法存入excel。转为Str类型。
+                    caseDict['ResultInfo'] = str(result)
+
+                if method == 'Get':
+                    response = requests.get(url, params=data, headers=headers)
+                    result = response.json()
+                    # 设置断言
+                    if result['code'] == expectValue['code'] and result['hint'] == expectValue['hint']:
+                        caseDict['CaseResult'] = 'Pass'
+                    else:
+                        caseDict['CaseResult'] = 'Fail'
+                    print('返回结果', end=': ')
+                    print(result)
+                    print('执行结果', end=': ')
+                    print(caseDict['CaseResult'], '\n')
+                    # 这里要对结果进行一下处理，要不无法存入excel。转为Str类型。
+                    caseDict['ResultInfo'] = str(result)
+            else:
+                print('返回结果: 用例设置无需执行，故没有执行！', '\n')
+            resultList.append(caseDict)
+            # print(resultList)
+        self.Ex.writeExcel('ComPrice', titleList, resultList)
+
+
+
+    def atestComPrice(self):
         '''比价神器'''
         print('比价神器', '\n')
 
@@ -39,13 +106,19 @@ class WmApiTest(unittest.TestCase):
             caseNum = caseDict['CaseNum']
             method = caseDict['Method']
             caseData = caseDict['CaseData']   # 这样获取的数据 是str 类型的
-            caseDataDict = eval(caseData)              # 使用eval 转换为dict 类型
+            if caseData != '':
+                caseDataDict = eval(caseData)              # 使用eval 转换为dict 类型
             # 将dict类型转化为json类型
             data = json.dumps(caseDataDict)
             caseRun = caseDict['CaseRun']
             caseName = caseDict['CaseName']
             # 设置请求头
-            headers = {'content-type': 'application/json; charset=utf8'}
+            # headers = {'content-type': 'application/json; charset=utf8'}
+            # 设置请求头
+            headers = caseDict['Header']
+            if headers != '':
+                headers = eval(headers)
+
             # 设置报告打印内容
             print('用例 ' + caseNum, end=': ')
             print(caseName)
@@ -100,9 +173,14 @@ class WmApiTest(unittest.TestCase):
             caseNum = caseDict['CaseNum']
             method = caseDict['Method']
             caseData = caseDict['CaseData']   # 这样获取的数据 是str 类型的
-            data = eval(caseData)              # 使用eval 转换为dict 类型
+            if caseData !='':
+                data = eval(caseData)              # 使用eval 转换为dict 类型
             caseRun = caseDict['CaseRun']
             caseName = caseDict['CaseName']
+            # 设置请求头
+            headers = caseDict['Header']
+            if headers != '':
+                headers = eval(headers)
             # 设置报告打印内容
             print('用例 ' + caseNum, end=': ')
             print(caseName)
@@ -111,7 +189,7 @@ class WmApiTest(unittest.TestCase):
             if caseRun == 'Y':
                 expectValue = eval(caseDict['ExpectValue'])
                 if method == 'Post':
-                    response = requests.post(url, data)
+                    response = requests.post(url, data,headers=headers)
                     result = response.json()
                     # 设置断言
                     if result['success'] == expectValue['success'] and result['error'] == expectValue['error']:
